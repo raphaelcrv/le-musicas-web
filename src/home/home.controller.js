d9dbec88d59a3c7e28283f7cc9405fc914ad301a
Raphael <raphaelcrv@hotmail.com>
2017-05-13 13:45:20 -0300

    home inegration api

--- src/home/home.controller.js
+++ src/home/home.controller.js
@@ -2,7 +2,38 @@ angular
   .module('app')
   .component('home', {
     templateUrl: 'home/home.html',
-    controller: function ($scope) {
+    controller: function ($scope, $http) {
       this.hello = 'Hello World! Hme!';
+      //console.log('loaded');
+      $scope.searchValue = '';
+
+      // todo - criar service para request
+      $scope.search = function(){
+        $scope.loading = true;
+        $http.get("https://api.vagalume.com.br/search.excerpt?q="+$scope.searchValue+"&limit=5")
+        .then(function(response) {          
+            // todo - tratar os erros 
+            $scope.data = response.data.response.docs;
+            $scope.loading = false;
+        });
+      }
+      // todo - criar service para request
+      //todo - criar constante api url
+      // https://api.vagalume.com.br/search.php?art=U2&mus=one&apikey={key}
+      $scope.lyrics = '';
+      $scope.getLyrics = function(){
+        console.log('lycris')
+        $http.get("https://api.vagalume.com.br/search.php?art=Twenty One Pilots&mus=Stressed Out&apikey={c9b424edc82806e396757c5c65958d51}")
+        .then(function(response) {          
+          $scope.lyrics = response.data.mus[0].text;
+          console.log($scope.lyrics);
+
+          $scope.teste = $scope.lyrics.replace(/(\r\n|\n|\r)/gm, " <br/>")
+          console.log($scope.teste)
+        });
+
+      }
+
+
     }
   });
