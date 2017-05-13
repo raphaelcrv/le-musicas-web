1e271db9664553ea38ec3398a117ce46b8da248f
Raphael <raphaelcrv@hotmail.com>
2017-05-13 13:35:15 -0300

    update routes

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,13 @@ angular
 /** @ngInject */
 function routesConfig($stateProvider, $urlRouterProvider, $locationProvider) {
   $locationProvider.html5Mode(true).hashPrefix('!');
-  $urlRouterProvider.otherwise('/');
+  //todo - create a 404 page and add to otherwise
+  $urlRouterProvider.otherwise('/home');
   
   // home page
   $stateProvider
     .state('home', {
-      url: '/',
+      url: '/home',
       component: 'home'
     })
 }
