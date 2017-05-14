0b736a2d54152d51d7edf6ac27ac49170109ddd9
Raphael <raphaelcrv@hotmail.com>
2017-05-14 00:28:12 -0300

    search mvc function

--- src/home/search/search.controller.js
+++ src/home/search/search.controller.js
@@ -2,7 +2,22 @@ angular
   .module('app')
   .component('search', {
     templateUrl: 'home/search/search.html',
-    controller: function () {
+    controller: function ($stateParams, $scope, $http, $state) {
 			this.hello = 'Hello World! Hme!';
+
+      if($stateParams.search != null){
+        $scope.loading = true;
+        $http.get("https://api.vagalume.com.br/search.excerpt?q="+$stateParams.search+"&limit=10")
+        .then(function(response) {          
+            // todo - tratar os erros 
+            $scope.results = response.data.response.docs;
+            $scope.loading = false;
+            console.log($scope.results);
+        });
+      }else{
+        $state.go('home.popular');
+      }
+
+
     }
 });
