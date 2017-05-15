d2e7087fefc2f7473412fa3247d8a484c4f8a9d1
Raphael <raphaelcrv@hotmail.com>
2017-05-14 22:09:13 -0300

    improvements lyrics controller and view

--- src/home/lyrics/lyrics.controller.js
+++ src/home/lyrics/lyrics.controller.js
@@ -3,8 +3,7 @@ angular
   .component('lyrics', {
 		templateUrl: 'home/lyrics/lyrics.html',
     controller: function ($http, $stateParams, $scope, $state) {
-      this.hello = 'Hello World! Hme!';
-			console.log('lyrics');
+      this.hello = 'Hello World! lyrics!';
 
 			if($stateParams.title != null && $stateParams.band != null ){
 				// creating obj music
@@ -13,17 +12,23 @@ angular
 					band : $stateParams.band
 				}
 
+				$scope.loading = true;
 				// request return full lyrics
 				$http.get("https://api.vagalume.com.br/search.php?art="+$scope.music.band+"&mus="+$scope.music.title+"&apikey={c9b424edc82806e396757c5c65958d51}")
-				.then(function(response) {          
-					$scope.lyrics = response.data.mus[0].text;
-					console.log($scope.lyrics);
-					// replace all breaklines from tag </br>
-					$scope.lyrics = $scope.lyrics.replace(/(\r\n|\n|\r)/gm, " <br/>")
-					console.log($scope.lyrics)
+				.then(function(response) {
+					$scope.loading = false;
+					// check if exists mus property
+					if(response.data.hasOwnProperty("mus")){
+    				$scope.lyrics = response.data.mus[0].text;
+						// replace all breaklines from tag </br>
+						$scope.lyrics = $scope.lyrics.replace(/(\r\n|\n|\r)/gm, " <br/>")
+						$scope.error = false;
+					}else{
+						$scope.error = true;
+					}
 				});
 			}else{
-				$state.go('home.popular')
+				$state.go('home')
 			}
 
 		}
