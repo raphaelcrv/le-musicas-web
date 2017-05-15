31a373c411e73487ba3607abc710466b5718f920
Raphael <raphaelcrv@hotmail.com>
2017-05-14 22:09:38 -0300

    improvements search controller and view

--- src/home/search/search.controller.js
+++ src/home/search/search.controller.js
@@ -5,17 +5,19 @@ angular
     controller: function ($stateParams, $scope, $http, $state) {
 			this.hello = 'Hello World! Hme!';
 
-      if($stateParams.search != null){
+      if($stateParams.search != undefined){
         $scope.loading = true;
         $http.get("https://api.vagalume.com.br/search.excerpt?q="+$stateParams.search+"&limit=10")
-        .then(function(response) {          
-            // todo - tratar os erros 
+        .then(function(response) {    
             $scope.results = response.data.response.docs;
             $scope.loading = false;
-            console.log($scope.results);
+            $scope.isEmpty = false;
+            if($scope.results.length == 0){
+              $scope.isEmpty = true;
+            }            
         });
       }else{
-        $state.go('home.popular');
+        $state.go('home');
       }
 
       $scope.showLyrics = function (title, band){
